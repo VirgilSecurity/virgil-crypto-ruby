f5e70befecc0402f7636e6929f73275d5be7dbdb
Vasilina <marfachaiko@gmail.com>
2019-02-28 14:30:54 +0500

    virgil core crypto tests

--- test/native/cipher_test.rb
+++ test/native/cipher_test.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2016 Virgil Security Inc.
+# Copyright (C) 2015-2019 Virgil Security Inc.
 #
 # Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
 #
@@ -8,17 +8,17 @@
 # modification, are permitted provided that the following conditions are
 # met:
 #
-#     (1) Redistributions of source code must retain the above copyright
-#     notice, this list of conditions and the following disclaimer.
+#   (1) Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
 #
-#     (2) Redistributions in binary form must reproduce the above copyright
-#     notice, this list of conditions and the following disclaimer in
-#     the documentation and/or other materials provided with the
-#     distribution.
+#   (2) Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in
+#   the documentation and/or other materials provided with the
+#   distribution.
 #
-#     (3) Neither the name of the copyright holder nor the names of its
-#     contributors may be used to endorse or promote products derived from
-#     this software without specific prior written permission.
+#   (3) Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
 #
 # THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
 # IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -26,11 +26,12 @@
 # DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
 # INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 # (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# SERVICES; LOSS OF USE, bytes, OR PROFITS; OR BUSINESS INTERRUPTION)
 # HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
 # STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
 # IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
+
 require 'test_helper'
 
 module Virgil
@@ -38,17 +39,17 @@ module Virgil
     class CipherTest < Minitest::Test
       def test_encrypts_and_decrypts_data
         raw_data = Bytes.from_string("test")
-        key_pair1 = Native::VirgilKeyPair.generate(
-          Native::VirgilKeyPair::Type_FAST_EC_ED25519
+        key_pair1 = Core::VirgilKeyPair.generate(
+          Core::VirgilKeyPair::Type_FAST_EC_ED25519
         )
-        key_pair2 = Native::VirgilKeyPair.generate(
-          Native::VirgilKeyPair::Type_FAST_EC_ED25519
+        key_pair2 = Core::VirgilKeyPair.generate(
+          Core::VirgilKeyPair::Type_FAST_EC_ED25519
         )
-        cipher = Native::VirgilCipher.new
+        cipher = Core::VirgilCipher.new
         cipher.add_key_recipient(Bytes.from_string("1"), key_pair1.public_key)
         cipher.add_key_recipient(Bytes.from_string("2"), key_pair2.public_key)
-        encrypted_data = cipher.encrypt(raw_data)
-        cipher = Native::VirgilCipher.new
+        encrypted_data = cipher.encrypt(raw_data, true)
+        cipher = Core::VirgilCipher.new
         decrypted_data1 = cipher.decrypt_with_key(
           encrypted_data,
           Bytes.from_string("1"),
