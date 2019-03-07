f609414c52404308f0b4de2cf978cb6302a75bca
Vasilina <marfachaiko@gmail.com>
2019-03-07 15:15:12 +0500

    crypto compatibility test

--- test/crypto_test.rb
+++ test/crypto_test.rb
@@ -36,73 +36,76 @@ require 'test_helper'
 module Virgil
   module Crypto
     class CryptoTest < Minitest::Test
+      def setup
+        @crypto = VirgilCrypto.new
+        @message_bytes = Bytes.from_string('hi')
+      end
+
       def test_generate_hash_generates_non_empty_array
-        crypto = VirgilCrypto.new
-        hash = crypto.generate_hash(Bytes.from_string('hi'))
+        hash = @crypto.generate_hash(@message_bytes)
         assert(hash)
       end
 
       def test_decrypt_encrypted_message_returns_equivalent_message
-        crypto = VirgilCrypto.new
-        key_pair = crypto.generate_keys
-        message_bytes = Bytes.from_string('hi')
-        encrypted = crypto.encrypt(message_bytes, key_pair.public_key)
-        assert_equal(message_bytes, crypto.decrypt(encrypted, key_pair.private_key))
+        key_pair = @crypto.generate_keys
+        encrypted = @crypto.encrypt(@message_bytes, key_pair.public_key)
+        assert_equal(@message_bytes, @crypto.decrypt(encrypted, key_pair.private_key))
       end
 
       def test_decrypt_encrypted_message_by_generated_from_key_materia_returns_equivalent_message
-        crypto = VirgilCrypto.new
         key_material = Bytes.from_string('26dfhvnslvdsfkdfvnndsb234q2xrFOuY5EDSAFGCCXHCJSHJAD')
-        key_pair = crypto.generate_keys(key_material: key_material)
-        message_bytes = Bytes.from_string('hi')
-        encrypted = crypto.encrypt(message_bytes, key_pair.public_key)
-        assert_equal(message_bytes, crypto.decrypt(encrypted, key_pair.private_key))
+        key_pair = @crypto.generate_keys(key_material: key_material)
+        encrypted = @crypto.encrypt(@message_bytes, key_pair.public_key)
+        assert_equal(@message_bytes, @crypto.decrypt(encrypted, key_pair.private_key))
 
       end
 
-      def test_decrypt_encrypted_message_with_wrong_key_raises_exception
-        crypto = VirgilCrypto.new
-        alice_key_pair = crypto.generate_keys
-        bob_key_pair = crypto.generate_keys
+      def test_sign_then_encrypt_decrypt_then_verify
+        author_key_pair = @crypto.generate_keys
+        receivers = [@crypto.generate_keys, @crypto.generate_keys]
+        encrypted = @crypto.sign_then_encrypt(@message_bytes, author_key_pair.private_key,
+                                              receivers[0].public_key, receivers[1].public_key)
+        receivers.each do |receiver|
+          decrypted = @crypto.decrypt_then_verify(encrypted, receiver.private_key, author_key_pair.public_key)
+          assert_equal(@message_bytes, decrypted)
+        end
+      end
 
-        message_bytes = Bytes.from_string('hi')
-        encrypted_for_alice = crypto.encrypt(message_bytes, alice_key_pair.public_key)
+      def test_decrypt_encrypted_message_with_wrong_key_raises_exception
+        alice_key_pair = @crypto.generate_keys
+        bob_key_pair = @crypto.generate_keys
+
+        encrypted_for_alice = @crypto.encrypt(@message_bytes, alice_key_pair.public_key)
         assert_raises VirgilCryptoException do
-          crypto.decrypt(encrypted_for_alice, bob_key_pair.private_key)
+          @crypto.decrypt(encrypted_for_alice, bob_key_pair.private_key)
         end
       end
 
       def test_generate_signature_returns_valid_signature
-        crypto = VirgilCrypto.new
-        key_pair = crypto.generate_keys
-        message_bytes = Bytes.from_string('some card snapshot')
-        signature = crypto.generate_signature(message_bytes, key_pair.private_key)
-        assert_equal(true, crypto.verify_signature(signature, message_bytes, key_pair.public_key))
+        key_pair = @crypto.generate_keys
+        signature = @crypto.generate_signature(@message_bytes, key_pair.private_key)
+        assert_equal(true, @crypto.verify_signature(signature, @message_bytes, key_pair.public_key))
       end
 
       def test_generate_stream_signature_returns_valid_signature
-        crypto = VirgilCrypto.new
-        key_pair = crypto.generate_keys
-        message_bytes = Bytes.from_string('some card snapshot')
-        stream = StringIO.new(message_bytes.to_s)
-        signature = crypto.generate_stream_signature(stream, key_pair.private_key)
+        key_pair = @crypto.generate_keys
+        stream = StringIO.new(@message_bytes.to_s)
+        signature = @crypto.generate_stream_signature(stream, key_pair.private_key)
         stream.rewind
-        assert_equal(true, crypto.verify_stream_signature(signature, stream, key_pair.public_key))
+        assert_equal(true, @crypto.verify_stream_signature(signature, stream, key_pair.public_key))
       end
 
       def test_decrypt_encrypted_stream_returns_equivalent_stream
-        crypto = VirgilCrypto.new
-        key_pair = crypto.generate_keys
-        message_bytes = Bytes.from_string('hi')
-        origin_stream = StringIO.new(message_bytes.to_s)
+        key_pair = @crypto.generate_keys
+        origin_stream = StringIO.new(@message_bytes.to_s)
         encrypted_stream = StringIO.new
-        crypto.encrypt_stream(origin_stream, encrypted_stream, key_pair.public_key)
+        @crypto.encrypt_stream(origin_stream, encrypted_stream, key_pair.public_key)
         decrypted_stream = StringIO.new
 
         encrypted_stream.rewind
-        crypto.decrypt_stream(encrypted_stream, decrypted_stream, key_pair.private_key)
+        @crypto.decrypt_stream(encrypted_stream, decrypted_stream, key_pair.private_key)
         decrypted_message_bytes = Bytes.from_string(decrypted_stream.string)
-        assert_equal(message_bytes, decrypted_message_bytes)
+        assert_equal(@message_bytes, decrypted_message_bytes)
       end
 
     end
